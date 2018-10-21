9a9b97b679d82c51e9413ebc29b41322e7655ea3
Ned <epe2@illinois.edu>
2018-10-21 00:11:15 -0500

    Who am i

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <wiringPiI2C.h>
 
 #define MPU_ADDRESS     0x68
+#define WHOAMI          0x68
 
 using namespace std;
 
@@ -14,23 +15,5 @@ int main()
 
    cout << "Init result: "<< fd << endl;
 
-   for(int i = 0; i < 0x0000ffff; i++)
-   {
-      // I tried using the "fast write" command, but couldn't get it to work.  
-      // It's not entirely obvious what's happening behind the scenes as
-      // regards to endianness or length of data sent.  I think it's only 
-      // sending one byte, when we really need two.
-      //
-      // So instead I'm doing a 16 bit register access.  It appears to 
-      // properly handle the endianness, and the length is specified by the 
-      // call.  The only question was the register address, which is the 
-      // concatenation of the command (010x = write DAC output) 
-      // and power down (x00x = power up) bits.
-      result = wiringPiI2CWriteReg16(fd, 0x40, (i & 0xfff) );
-
-      if(result == -1)
-      {
-         cout << "Error.  Errno is: " << errno << endl;
-      }
-   }
+   cout << "Who Am I " << hex << wiringPiI2CReadReg16(fd, WHOAMI) << endl;
 }
